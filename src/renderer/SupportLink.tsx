f4a956f50190ec37c759b331def172f89d54464b
Carter <cdw77@drexel.edu>
2021-01-28 17:03:42 -0500

    Update support link

--- src/renderer/SupportLink.tsx
+++ src/renderer/SupportLink.tsx
@@ -10,7 +10,7 @@ const SupportLink: React.FC = function () {
 			<Link
 				href="#"
 				color="secondary"
-				onClick={() => shell.openExternal('https://discord.gg/fFurRtJsZj')}
+				onClick={() => shell.openExternal('https://www.github.com/impostietalkie/Crewlink-mobile/issues')}
 			>
 				Get support
 			</Link>
